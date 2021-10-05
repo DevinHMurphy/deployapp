c34917912bdfd0782d196dd59fd48c3c65da9dfb
DevinHMurphy <[demurphy1324@gmail.com]>
2021-10-05 17:41:32 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '134.209.210.82', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
